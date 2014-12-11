13fddce3a736d7c290dbef7ecd438f1f8e0a342d
Zhenchao Yan <yanzhenchao@gmail.com>
2014-12-10 20:46:49 -0500

    Updated nvm installation
    The original url does not anymore

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
